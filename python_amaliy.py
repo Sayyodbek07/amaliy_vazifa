a0660e00cf2534028dcb580f5569099663aedd40
Sayyodbek <sarvinoztogayeva603@gmail.com>
2025-10-04 19:09:26 +0500

    finish the imthon

--- python_amaliy.py
+++ python_amaliy.py
@@ -23,3 +23,16 @@
 # }
 #
 # print(dict_to_list(data))
+
+
+# CREATE TABLE Products (
+# 	product_id INT,
+#     product_name VARCHAR(100),
+#     product_type VARCHAR(100),
+#     product_price CHAR(50)
+# );
+#
+# SELECT *
+# FROM Products
+#
+# INSERT INTO Products (product_id, product_name, product_type, product_price) VALUES (1, 'Truckcar', 'bolalar oyinchogi', '25$'), (2, 'kompyuter', 'bolalar o’yinchoqi', '15 $'), (3, 'Noutbook', 'bolalar oyinchoqi', '1000$'), (4, 'Smartphone', 'bolalar oyinchoqi', '500$'), (5, 'televizor', 'bolalar oyinchoqi', '1000$');
