f1b0d856278f6d9a9d965cfc3fd5d0e7b3d7bda7
Sayyodbek <sarvinoztogayeva603@gmail.com>
2025-10-04 19:02:46 +0500

    amaliy vazifalar

---
+++ python_amaliy.py
@@ -0,0 +1,25 @@
+# def dict_to_list(d):
+#     return d
+#
+# data = {
+#   "D": 1,
+#   "B": 2,
+#   "C": 3
+# }
+#
+# print(dict_to_list(data))
+#
+
+
+
+# 2 vazifa
+# def dict_to_list(d):
+#     return d
+#
+# data = {
+#   "D": 1,
+#   "B": 2,
+#   "C": 3
+# }
+#
+# print(dict_to_list(data))
